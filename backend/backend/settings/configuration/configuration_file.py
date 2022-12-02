9c396fb680cc729642d741446c0395409b9e44e1
Mustafa <git@ms7m.me>
2022-12-01 21:58:57 -0600

    feat: add version and environment to configuration_file

--- backend/backend/settings/configuration/configuration_file.py
+++ backend/backend/settings/configuration/configuration_file.py
@@ -3,11 +3,15 @@
 from proper_env_file.finder import determine_valid_environment_file
 from pydantic import BaseSettings
 
+
+APPLICATION_VERSION = open(".version").read().strip()
+fetch_environment_mode, fetch_environment_file = determine_valid_environment_file()
+
 class BackendSettings(BaseSettings):
     class Config:
         """Config for BackendSettings."""
         
-        env_file = determine_valid_environment_file()[1]
+        env_file = fetch_environment_file
         env_file_encoding = "utf-8"
     
     celery_redis_backend_host: str
@@ -17,3 +21,15 @@ class BackendSettings(BaseSettings):
     celery_redis_broker_host: str
     celery_redis_broker_port: int
     celery_redis_broker_db: int
+
+
+class VersionInformation(BaseSettings):
+    class Config:
+        """Config for VersionInformation."""
+        
+        env_file = "version.env"
+        env_file_encoding = "utf-8"
+    
+    version: str
+    version_date: str
+    version_commit: str
