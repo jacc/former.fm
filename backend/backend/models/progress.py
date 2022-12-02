d3f4194d6378bb4ae7ca8159a4976368dda851ae
Mustafa <git@ms7m.me>
2022-12-01 21:58:46 -0600

    fix: properly hint expected celery results

--- backend/backend/models/progress.py
+++ backend/backend/models/progress.py
@@ -1,30 +1,31 @@
-
 import enum
 from pydantic import BaseModel, Json
 import typing
 from celery.states import STARTED, SUCCESS, FAILURE, REVOKED, RETRY, PENDING
+from .tracks import LastFmTrack, UnprocessedLastFmScrobbles
 
 
 class CeleryTasks(str, enum.Enum):
     """Celery tasks"""
+
     #: Task state is unknown (assumed pending since you know the id).
-    PENDING = 'PENDING'
+    PENDING = "PENDING"
     #: Task was received by a worker (only used in events).
-    RECEIVED = 'RECEIVED'
+    RECEIVED = "RECEIVED"
     #: Task was started by a worker (:setting:`task_track_started`).
-    STARTED = 'STARTED'
+    STARTED = "STARTED"
     #: Task succeeded
-    SUCCESS = 'SUCCESS'
+    SUCCESS = "SUCCESS"
     #: Task failed
-    FAILURE = 'FAILURE'
+    FAILURE = "FAILURE"
     #: Task was revoked.
-    REVOKED = 'REVOKED'
+    REVOKED = "REVOKED"
     #: Task was rejected (only used in events).
-    REJECTED = 'REJECTED'
+    REJECTED = "REJECTED"
     #: Task is waiting for retry.
-    RETRY = 'RETRY'
-    IGNORED = 'IGNORED'
-    PROGRESS = 'PROGRESS'
+    RETRY = "RETRY"
+    IGNORED = "IGNORED"
+    PROGRESS = "PROGRESS"
 
 
 class BackgroundProcessingStates(int, enum.Enum):
@@ -34,25 +35,35 @@ class BackgroundProcessingStates(int, enum.Enum):
     PROCESSING_TRACKS = 3
     COMPLETE = 4
     FAILED = 5
-    
-    
+
+
 class OptionalTaskMetaInformation(BaseModel):
     tracks_collected_so_far: typing.Optional[int]
     task_started_at: typing.Optional[str]
     estimated_tracks_to_collect: typing.Optional[int]
-    
-    
-    
+
+
 class MetaState(BaseModel):
     status: BackgroundProcessingStates
     progress: int
     meta: typing.Optional[OptionalTaskMetaInformation]
-    
 
+
+class ProcessedLastFmTrackResponse(BaseModel):
+    status: typing.Literal["processed"]
+    data: typing.Dict[str, LastFmTrack]
+
+
+class UnprocessedLastFmTrackResponse(BaseModel):
+    status: typing.Literal["unprocessed"]
+    data: typing.Dict[str, UnprocessedLastFmScrobbles]
 
 
 class CompletedCeleryResult(BaseModel):
     task_id: str
     status: CeleryTasks
-    result: typing.Optional[dict]
-    
+    result: typing.Optional[
+        typing.Union[
+            MetaState, ProcessedLastFmTrackResponse, UnprocessedLastFmTrackResponse
+        ]
+    ]
