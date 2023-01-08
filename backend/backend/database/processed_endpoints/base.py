972e869895208e249860a65f7c145d87330896f2
Mustafa Mohamed <git@ms7m.me>
2023-01-07 22:14:29 -0600

    fix: use str hint for ``time`` attribute in BaseSavedDataInformationDatabase

--- backend/backend/database/processed_endpoints/base.py
+++ backend/backend/database/processed_endpoints/base.py
@@ -7,7 +7,10 @@ from backend.models.types.arrow_time import ArrowISODatetime
 import typing
 
 class BaseSavedDataInformationDatabase(BaseModel):
+    class Settings:
+        name = "user_saved_data"
+        
     status: typing.Literal["processed", "unprocessed"]
     data: typing.Dict[str, typing.Union[LastFmTrack, UnprocessedLastFmScrobbles]]
     username: str
-    time: ArrowISODatetime
+    time: str
