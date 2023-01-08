0ed9299bfe4bfd501695ba189cbd738cee1c7be9
Mustafa Mohamed <git@ms7m.me>
2023-01-07 22:12:31 -0600

    fix: add base function for blocking dupe requests (checks redis)

--- backend/backend/api/depends/prevent_duplication.py
+++ backend/backend/api/depends/prevent_duplication.py
@@ -11,9 +11,19 @@ redis = Redis(
 )
 
 
-async def block_duplicate_requests(username_to_fetch: str):
+async def _base_duplicate_requests(username_to_fetch: str):
     """Block duplicate requests."""
     _fetch_account = await redis.get(username_to_fetch)
+    
+    
+    if _fetch_account:
+        return _fetch_account.decode("utf-8")
+    else:
+        return False
+
+async def block_duplicate_requests(username_to_fetch: str):
+    """Block duplicate requests."""
+    _fetch_account = await _base_duplicate_requests(username_to_fetch)
     
     
     if _fetch_account:
@@ -21,7 +31,7 @@ async def block_duplicate_requests(username_to_fetch: str):
             status_code=400,
             detail={
                 "message": "There is already a task running for this account",
-                "task_id": _fetch_account.decode("utf-8"),
+                "task_id": _fetch_account,
             }
         )
     else:
